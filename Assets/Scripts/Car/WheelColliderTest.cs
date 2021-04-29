5aa00c7c6b9ccf27631e06b1d1ea7ed68a1355ce
Frederik Riisager Johnsen <frederik@familienjohnsen.dk>
2021-04-29 19:20:26 +0200

    followed unity wheel collider tutorial

---
+++ Assets/Scripts/Car/WheelColliderTest.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelColliderTest : MonoBehaviour
+{
+    [SerializeField] private List<AxleInfo> axleInfos;
+    [SerializeField] private float maxMotorTorque;
+    [SerializeField] private float maxSteeringAngle;
+    private InputManager inputManager;
+    void Start()
+    {
+        inputManager = InputManager.Instance;
+    }
+
+    void FixedUpdate()
+    {
+        float motor = maxMotorTorque * inputManager.GetAccelerator();
+        float steering = maxSteeringAngle * inputManager.GetSteering();
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if(axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+
+            if(axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
