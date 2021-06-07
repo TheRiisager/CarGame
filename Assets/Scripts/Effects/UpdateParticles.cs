b8d7977c9a2f61e4d00fa295c0cf631f1f240b08
Frederik Riisager Johnsen <frederik@familienjohnsen.dk>
2021-06-07 12:56:02 +0200

    finished particleFX

--- Assets/Scripts/Effects/UpdateParticles.cs
+++ Assets/Scripts/Effects/UpdateParticles.cs
@@ -8,24 +8,39 @@ public class UpdateParticles : MonoBehaviour
     [SerializeField] float particleCutoffVelocity = 1;
     VisualEffect[] vfx;
     Rigidbody rb;
-
+    [SerializeField] float tireTrackThreshold = 2f;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         vfx = GetComponentsInChildren<VisualEffect>();
     }
-    void Update()
+    void FixedUpdate()
     {
         Vector3 velocity = rb.velocity;
+        float localXVelocity = Mathf.Abs( transform.InverseTransformDirection(velocity).x );
         foreach(VisualEffect fx in vfx)
         {
-            fx.SetFloat("ExitVelocity", velocity.magnitude * 0.5f);
-            if(velocity.magnitude <= particleCutoffVelocity)
+            if(fx.GetInt("id") == 1)
             {
-                fx.enabled = false;
-            } else
+                fx.SetFloat("ExitVelocity", velocity.magnitude * 0.5f);
+                if(velocity.magnitude <= particleCutoffVelocity)
+                {
+                    fx.SetInt("spawnrate", 0);
+                } else
+                {
+                    fx.SetInt("spawnrate", 50);
+                }
+            }
+
+            if(fx.GetInt("id") == 2)
             {
-                fx.enabled = true;
+                if(localXVelocity >= tireTrackThreshold)
+                {
+                    fx.SetInt("spawnrate", 128);
+                } else
+                {
+                    fx.SetInt("spawnrate", 0);
+                }
             }
         }
     }
