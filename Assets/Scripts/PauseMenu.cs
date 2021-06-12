5fb0fe07fd67c55f353cb8f035944d9704c5b26c
Frederik Riisager Johnsen <frederik@familienjohnsen.dk>
2021-06-12 20:08:01 +0200

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused;
+    Menumanager menumanager;
+    [SerializeField] GameObject pauseMenuObject;
+    void start()
+    {
+        GameObject.FindGameObjectWithTag("MenuManager").GetComponent<Menumanager>();
+    }
+
+    void OnEnable()
+    {
+        InputManager.MenuButtonEvent += HandleMenuEvent;
+    }
+
+    void OnDisable()
+    {
+        InputManager.MenuButtonEvent -= HandleMenuEvent;
+    }
+
+    public void LoadMainMenu()
+    {
+        menumanager.LoadNewMenu("MainMenu");
+    }
+
+    public void ResetTrack()
+    {
+
+    }
+
+    private void HandleMenuEvent()
+    {
+        if(isPaused)
+        {
+            Resume();
+        } else
+        {
+            Pause();
+        }
+    }
+
+    private void Resume()
+    {
+        pauseMenuObject.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuObject.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+}
